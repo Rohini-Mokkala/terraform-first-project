4682d756f459d1712fd9eea8f808378bf4609e9e
Rohini-Mokkala <mokkalarohini.btech@gmail.com>
2025-07-18 04:25:57 +0000

    created

--- main.tf
+++ main.tf
@@ -1,3 +1,22 @@
 provider "azurerm" {
     features {}
 }
+
+resource "azurerm_resource_group" "Assignterra" {
+    name     = "Assignterra"
+    location = "East US"
+}
+ 
+resource "azurerm_storage_account" "storage" {
+  name                     = "msjsa18"
+  resource_group_name      = Assignterra.Assignterra.name
+  location                 = Assignterra.Assignterra.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+ 
+resource "azurerm_storage_container" "blob_container" {
+  name                  = "msjcontainer"
+  storage_account_name  = msj18.storage.name
+  container_access_type = "public"
+}
